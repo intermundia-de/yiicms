8d26dc60c58db3380aec05356afc2aaaa7b29751
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-07-23 14:03:28 +0400

    Fix bug in ContentEditingToolbar

--- widgets/ContentEditingToolbar.php
+++ widgets/ContentEditingToolbar.php
@@ -8,7 +8,14 @@
 namespace intermundia\yiicms\widgets;
 
 
+use frontend\modules\user\models\LoginForm;
+use intermundia\yiicms\models\ContentTree;
+use Yii;
+use yii\bootstrap\Alert;
 use yii\bootstrap\Widget;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\bootstrap\ActiveForm;
 
 /**
  * Class ContentEditingHeader
@@ -25,5 +32,63 @@ class ContentEditingToolbar extends Widget
         return $this->render('editing_toolbar.php', [
             'widget' => $this,
         ]);
+
+        if (\Yii::$app->user->canEditContent()) {
+
+            /** @var ContentTree $contentTreeObject */
+            $contentTreeObject = Yii::$app->pageContentTree;
+
+            return Alert::widget([
+                'closeButton' => false,
+                'options' => [
+                    'class' => 'alert alert-warning content-editor'
+                ],
+                'body' => '<div class="container">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <span>' . Yii::t('frontend', 'You are in the Content Editing mode') . '</span>
+                        </div>
+                        <div class="col-md-5 text-right">
+                            <label>
+                                <input id="with-hidden-checkbox" type="checkbox" name="with-hidden" ' .
+                    ( !Yii::$app->request->get('hidden') ? '' : 'checked' ) . ' > ' .
+                    Yii::t('frontend', 'With Hidden') .
+                    '</label>' .
+                    \yii\helpers\Html::a(Yii::t('frontend', 'Edit in backend'), $contentTreeObject->getBackendFullUrl()
+                        ,
+                        [
+                            'id' => 'to-backend-url',
+//                                        'data-backend-url' => Yii::getAlias('@backendUrl/content/website') . Yii::$app->request->url,
+//                                        'data-method' => 'post',
+                            'class' => 'btn btn-sm btn-warning btn-content-editing',
+                            'target' => '_blank'
+                        ]) .
+                    \yii\helpers\Html::a(Yii::t('frontend', 'Logout'),
+                        \yii\helpers\Url::to(['/user/sign-in/logout']),
+                        ['data-method' => 'post', 'class' => 'btn btn-sm btn-danger btn-content-editing btn-logout']) . '
+                        </div>
+                    </div>
+                </div>'
+            ]);
+        } else if ($this->showLogin) {
+
+        }
+    }
+
+    public function beginForm()
+    {
+
+        $form = ActiveForm::begin([
+            'id' => 'login-form',
+            'layout' => 'inline',
+            'action' => ['/user/sign-in/login']
+        ]);
+        $content = $form->field($model, 'identity');
+        $content .= $form->field($model, 'password')->passwordInput();
+        $content .= '<div class="form-group">' .
+            Html::submitButton(Yii::t('frontend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) .
+            '</div>';
+
+        return $content;
     }
 }
--- widgets/views/editing_toolbar.php
+++ widgets/views/editing_toolbar.php
@@ -15,57 +15,82 @@ use yii\helpers\Html;
 $model = new LoginForm();
 ?>
 
-<?php Alert::begin([
-    'closeButton' => false,
-    'options' => [
-        'class' => 'alert alert-warning content-editor'
-    ]
-]); ?>
+
+<?php if (Yii::$app->user->canEditContent()): ?>
+    <?php Alert::begin([
+        'closeButton' => false,
+        'options' => [
+            'class' => 'alert alert-warning content-editor'
+        ]
+    ]); ?>
     <div class="container">
         <div class="row">
+
             <div class="col-md-7">
-                <span><?php echo Yii::t('frontend', 'Enter Your username and password to enable frontend editing') ?></span>
+                <span>
+                    <?php echo Yii::t('frontend', 'You are in the Content Editing mode') ?>
+                </span>
             </div>
             <div class="col-md-5 text-right">
-                <?php if (!Yii::$app->user->canEditContent() && $widget->showLogin): ?>
-                    <?php $form = ActiveForm::begin([
-                        'id' => 'login-form',
-                        'layout' => 'inline',
-                        'action' => ['/user/sign-in/login']
-                    ]); ?>
-
-                    <?php echo $form->field($model, 'identity', [
-                        'inputOptions' => [
-                            'placeholder' => $model->attributeLabels()['identity']
-                        ]
-                    ]) ?>
-                    <?php echo $form->field($model, 'password', [
-                        'inputOptions' => [
-                            'placeholder' => $model->attributeLabels()['password']
-                        ]
-                    ])->passwordInput() ?>
-                    <div class="form-group">
-                        <?php echo Html::submitButton(Yii::t('frontend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                    </div>
-                    <?php ActiveForm::end() ?>
-                <?php else: ?>
-                    <label>
-                        <input id="with-hidden-checkbox" type="checkbox" name="with-hidden"
-                            <?php echo( !Yii::$app->request->get('hidden') ? '' : 'checked' ) ?>>
-                        <?php echo Yii::t('frontend', 'With Hidden') ?>
-                    </label> <?php echo \yii\helpers\Html::a(
+                <label>
+                    <input id="with-hidden-checkbox" type="checkbox" name="with-hidden"
+                        <?php echo( !Yii::$app->request->get('hidden') ? '' : 'checked' ) ?>>
+                    <?php echo Yii::t('frontend', 'With Hidden') ?>
+                </label> <?php echo \yii\helpers\Html::a(
                         Yii::t('frontend', 'Edit in backend'), Yii::$app->pageContentTree->getBackendFullUrl(),
                         [
                             'id' => 'to-backend-url',
                             'class' => 'btn btn-sm btn-warning btn-content-editing',
                             'target' => '_blank'
-                        ]) . '&nbsp'.
+                        ]) . '&nbsp' .
                     \yii\helpers\Html::a(Yii::t('frontend', 'Logout'),
                         \yii\helpers\Url::to(['/user/sign-in/logout']),
                         ['data-method' => 'post', 'class' => 'btn btn-sm btn-danger btn-content-editing btn-logout'])
-                    ?>
-                <?php endif; ?>
+                ?>
             </div>
         </div>
     </div>
-<?php Alert::end() ?>
+    <?php Alert::end() ?>
+
+<?php elseif ($widget->showLogin): ?>
+    <?php Alert::begin([
+        'closeButton' => false,
+        'options' => [
+            'class' => 'alert alert-warning content-editor'
+        ]
+    ]); ?>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-7">
+                <span>
+                    <?php echo Yii::t('frontend', 'Enter Your username and password to enable frontend editing') ?>
+                </span>
+            </div>
+            <div class="col-md-5 text-right">
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'layout' => 'inline',
+                    'action' => ['/user/sign-in/login']
+                ]); ?>
+
+                <?php echo $form->field($model, 'identity', [
+                    'inputOptions' => [
+                        'placeholder' => $model->attributeLabels()['identity']
+                    ]
+                ]) ?>
+                <?php echo $form->field($model, 'password', [
+                    'inputOptions' => [
+                        'placeholder' => $model->attributeLabels()['password']
+                    ]
+                ])->passwordInput() ?>
+                <div class="form-group">
+                    <?php echo Html::submitButton(Yii::t('frontend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+                <?php ActiveForm::end() ?>
+            </div>
+        </div>
+    </div>
+    <?php Alert::end() ?>
+
+<?php endif; ?>
