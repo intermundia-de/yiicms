b61fa155396e424056b884d88964dcef4ca19bd8
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-08-13 12:02:54 +0400

    Add SiteController to handle errors

---
+++ controllers/SiteController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace intermundia\yiicms\controllers;
+
+use intermundia\yiicms\web\BackendController;
+use Yii;
+
+/**
+ * Site controller
+ */
+class SiteController extends BackendController
+{
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    public function beforeAction($action)
+    {
+        $this->layout = Yii::$app->user->isGuest || !Yii::$app->user->can('loginToBackend') ? 'base' : 'common';
+
+        return parent::beforeAction($action);
+    }
+}
