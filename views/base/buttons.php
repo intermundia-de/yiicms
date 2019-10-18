7a158088de0b2613086158a321a7a89381a80656
miriani <mirianjinchvelashvili@gmail.com>
2019-10-18 12:55:51 +0400

    Change disable check logic as we have in v2 yiicms

--- views/base/buttons.php
+++ views/base/buttons.php
@@ -21,7 +21,7 @@ use yii\helpers\Html;
             'name' => 'go_to_parent',
             'value' => '1',
             'class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary',
-            'disabled' => $model->getModelClass()::getFormattedTableName() == \intermundia\yiicms\models\ContentTree::TABLE_NAME_WEBSITE
+            'disabled' => $model->contentTree && $model->contentTree->depth == 0
         ]) ?>
     <?php echo Html::submitButton(Yii::t('intermundiacms',
         $model->isNewRecord ? 'Create and stay' : 'Update and stay'),
